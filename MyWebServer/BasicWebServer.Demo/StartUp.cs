30fc103f945a72759b2cffc6bc677139c6b9b49f
boyan-ast <62556633+boyan-ast@users.noreply.github.com>
2022-01-13 18:24:19 +0200

    HttpServer class created

---
+++ MyWebServer/BasicWebServer.Demo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using BasicWebServer.Server;
+using System;
+
+namespace BasicWebServer.Demo
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
---
+++ MyWebServer/BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicWebServer.Server
+{
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress, this.port);
+        }
+
+        public void Start()
+        {
+            this.serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                TcpClient connection = this.serverListener.AcceptTcpClient();
+
+                NetworkStream networkStream = connection.GetStream();
+
+                string content = "Hello from the server!";
+                
+                this.WriteResponse(networkStream, content);
+
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            int contentLength = Encoding.UTF8.GetByteCount(message);
+
+            string responseString = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseString);
+
+            networkStream.Write(responseBytes);
+        }
+    }
+}
