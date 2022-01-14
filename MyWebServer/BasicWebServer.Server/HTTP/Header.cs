17d2726b870e5f14a6cb7da113bc2220a789285a
boyan-ast <boyan.m.ast@gmail.com>
2022-01-14 13:56:17 +0200

    Implemented reading request and printing them

--- MyWebServer/BasicWebServer.Server/HTTP/Header.cs
+++ MyWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -1,9 +1,14 @@
-﻿namespace BasicWebServer.Server.HTTP
+﻿using BasicWebServer.Server.Common;
+
+namespace BasicWebServer.Server.HTTP
 {
     public class Header
     {
         public Header(string name, string value)
         {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
             this.Name = name;
             this.Value = value;
         }
--- MyWebServer/BasicWebServer.Server/HttpServer.cs
+++ MyWebServer/BasicWebServer.Server/HttpServer.cs
@@ -31,12 +31,43 @@ namespace BasicWebServer.Server
 
                 NetworkStream networkStream = connection.GetStream();
 
+                string requestText = this.ReadRequest(networkStream);
+
+                Console.WriteLine(requestText);
+
                 string content = "Hello from the server!";
                 
                 this.WriteResponse(networkStream, content);
 
                 connection.Close();
             }
+        }
+
+        private string ReadRequest(NetworkStream networkStream)
+        {
+            int bufferLength = 1024;
+            byte[] buffer = new byte[bufferLength];
+
+            int totalBytes = 0;
+
+            StringBuilder requestBuilder = new StringBuilder();
+
+            do
+            {
+                int bytesRead = networkStream.Read(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10240)
+                {
+                    throw new InvalidOperationException("Request is too large");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+            } while (networkStream.DataAvailable);
+
+            return requestBuilder.ToString();
         }
 
         private void WriteResponse(NetworkStream networkStream, string message)
